0a0e3e8a2496af50894f266385993279bd5a897f
CheikhTidianeGaye <gaye95ahmeth@gmail.com>
2019-11-13 17:39:30 +0000

    merge

--- php/index.php
+++ php/index.php
@@ -1,12 +1,13 @@
 <?php
-   function factoriel($nombre){
-       $i=$nombre;
-       $valeur=1;
-     while ($i>1){
-         $valeur=$valeur*($i-1);
-         $i--;
-     }
-     return  $valeur;
-   }
-   factoriel(12);
+    function factoriel($nombre){
+        $i=$nombre;
+        $valeur=1;
+        while($i>1){
+            $valeur=$valeur*($i);
+            $i--;
+        }
+        return $valeur;
+    }
+    echo factoriel(12);
+
 ?>
